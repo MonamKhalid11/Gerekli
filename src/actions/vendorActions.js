c2f90092502f1643716b3739b6e955de43b29016
Gubanov Igor <igubanov@cs-cart.com>
2020-09-25 15:55:50 +0300

    [~]{@1-27038} Made an action to get comments.

--- src/actions/vendorActions.js
+++ src/actions/vendorActions.js
@@ -17,6 +17,27 @@ import {
 } from '../constants';
 import Api from '../services/api';
 
+export function getDiscussion(id, type, params) {
+  return async (dispatch) => {
+    try {
+      const discussionResult = await Api.get(`/sra_discussion/?object_type=${type}&object_id=${id}&params[page]=${params.page}`);
+      dispatch({
+        type: FETCH_DISCUSSION_SUCCESS,
+        payload: {
+          id: `${type.toLowerCase()}_${id}`,
+          page: params.page,
+          discussion: discussionResult.data,
+        },
+      });
+    } catch (error) {
+      dispatch({
+        type: FETCH_DISCUSSION_FAIL,
+        error,
+      });
+    }
+  };
+}
+
 export function fetch(id, type, params) {
   return async (dispatch) => {
     dispatch({
@@ -35,6 +56,8 @@ export function fetch(id, type, params) {
         error,
       });
     }
+
+    // getDiscussion(id, type, params)(dispatch);
 
     try {
       const discussionResult = await Api.get(`/sra_discussion/?object_type=${type}&object_id=${id}&params[page]=${params.page}`);
