debf201a51e1213589ea1a16eb8ead01357282f3
Amelekesov <aleksey.melekesov@rt.ru>
2020-09-30 17:22:11 +0400

    Refactoring.

--- ios/csnativeTests/multivendorTests.m
+++ ios/csnativeTests/multivendorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface multivendorTests : XCTestCase
+@interface csnativeTests : XCTestCase
 
 @end
 
-@implementation multivendorTests
+@implementation csnativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
