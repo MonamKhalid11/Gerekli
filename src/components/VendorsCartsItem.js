e2241cee72b7fb1281e7ea43db958928f18269a7
Gubanov Igor <igubanov@cs-cart.com>
2020-10-07 12:15:23 +0300

    [~]{@1-26037} Refactored vendor`s cart name.

--- src/components/VendorsCartsItem.js
+++ src/components/VendorsCartsItem.js
@@ -50,7 +50,7 @@ const VendorsCartsItem = ({
 
   return (
     <View style={styles.container}>
-      {item.product_groups[0] && renderHeader(item.product_groups[0].name)}
+      {item.cart_name && renderHeader(item.cart_name)}
       {cartIsOpen && (
         <CartProductList
           cart={item}
